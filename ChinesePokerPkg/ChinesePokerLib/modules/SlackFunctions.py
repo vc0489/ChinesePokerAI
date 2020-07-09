e65213d48d7c78a219dc884d694692b1d836e868
Vincent Chen <vc0489@gmail.com>
2020-07-09 15:50:40 +0100

    Moved to modules sub-package and generalised function to send message to channel

---
+++ ChinesePokerPkg/ChinesePokerLib/modules/SlackFunctions.py
@@ -0,0 +1,36 @@
+#%%
+import requests
+from pathlib import Path
+import json
+import os
+import sys
+
+#%%
+
+#slack_token = os.environ['SLACK_TOKEN']
+#slack_channel = '#chinese-poker'
+slack_icon = 'https://img.icons8.com/color/96/000000/cards.png'
+slack_user_name = 'Chokerot'
+
+def post_message_to_slack_channel(text, channel, token=None, icon=None, username=None, blocks=None):
+  
+  if slack_token is None:
+    slack_token = os.environ['SLACK_TOKEN']
+  return requests.post('https://slack.com/api/chat.postMessage', {
+    'token': token,
+    'channel': channel,
+    'text': text,
+    'icon_url': icon,
+    'username': username,
+    'blocks': json.dumps(blocks) if blocks else None
+  }).json()
+
+
+
+if __name__ == "__main__":
+  msg = 'Testing. 1,2,3.'
+  post_message_to_slack(msg)
+  sys.exit()
+
+
+# %%
